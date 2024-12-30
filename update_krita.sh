a91452c910d42bb3d1d4d3acc852fcc85ca80976
Sam Van Hulle <sam@rombiezobots.com>
2024-12-30 23:12:05 +0100

    added Krita update script

---
+++ update_krita.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+URL_BASE="https://download.kde.org/Attic/krita/"
+DIRECTORY_DOWNLOAD="$HOME/Downloads"
+DIRECTORY_INSTALL="/opt/krita"
+DIRECTORY_RUN="/usr/local/bin"
+
+sudo mkdir -p $DIRECTORY_INSTALL
+
+# Determine the latest version directory on the Krita website.
+VERSION_FOLDER=$(curl -s $URL_BASE | grep -oP '(?<=href=")\d+\.\d+\.\d+\/(?=")' | sort -V | tail -n 1)
+URL_PAGE="${URL_BASE}${VERSION_FOLDER}"
+
+# Construct the download URL for the latest release AppImage.
+NAME_APPIMAGE=$(curl -s $URL_PAGE | grep -oP '(?<=href=")[^"]+x86_64\.appimage(?=")' | sort -V | tail -n 1)
+URL_APPIMAGE="${URL_PAGE}${NAME_APPIMAGE}"
+echo "Latest is $URL_APPIMAGE"
+
+DOWNLOAD_APPIMAGE="$DIRECTORY_DOWNLOAD/$NAME_APPIMAGE"
+INSTALL_APPIMAGE="$DIRECTORY_INSTALL/$NAME_APPIMAGE"
+
+# Exit if the version is already installed.
+if [[ -d $INSTALL_APPIMAGE ]]; then
+    echo "$INSTALL_APPIMAGE is already installed."
+    exit
+fi
+
+# Download the latest version.
+if ! [[ -f $DOWNLOAD_APPIMAGE ]]; then
+    echo "Downloading $URL_APPIMAGE..."
+    curl -L -o $DOWNLOAD_APPIMAGE $URL_APPIMAGE
+    if [[ -f $DOWNLOAD_APPIMAGE ]]; then
+        echo "Downloaded $DOWNLOAD_APPIMAGE"
+    else
+        echo "Failed to download Krita. Please check the URL or network connection."
+    fi
+else
+    echo "$DOWNLOAD_APPIMAGE has already been downloaded."
+fi
+
+# Allow executing and move the AppImage to the install directory.
+chmod +x $DOWNLOAD_APPIMAGE
+sudo mv $DOWNLOAD_APPIMAGE $INSTALL_APPIMAGE
+echo "Moved AppImage to $INSTALL_APPIMAGE"
+
+# Update the symbolic link.
+sudo ln -sf "$INSTALL_APPIMAGE" "$DIRECTORY_RUN/krita"
+echo "Updated symbolic link $DIRECTORY_RUN/krita -> $INSTALL_APPIMAGE"
